589a4f6a9a6e9af3f3ef51e3162dcabcdae0a59d
ben476 <benjamin.hong476@gmail.com>
2018-09-04 11:51:10 +1200

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var dice2 = 0;
 var sum1 = 0;
 var sum2 = 0;
 var currentplayer = 1;
+var numbertoplayto = prompt("What number do you ant to play to?", 100);
 function diceroll(dice1, dice2) {
     dice1 = Math.round(Math.floor((Math.random() * 6.49) + 0.5));
     dice2 = Math.round(Math.floor((Math.random() * 6.49) + 0.5));
@@ -30,13 +31,28 @@ else if (currentplayer == 2) {
     confirm("Snake eyes!")
 }
 }
+    
 if (currentplayer == 1) {
+    if (sum1<numbertoplayto) {
     sum1 = sum1 + dice1 + dice2;
+    }
+    if (sum1>numbertoplayto) {
+    sum1 = sum1 - dice1 - dice2;
+    }
 }
 else if (currentplayer == 2) {
+    if (sum2<numbertoplayto) {
     sum2 = sum2 + dice1 + dice2;
+    }
+    if (sum2>numbertoplayto) {
+    sum2 = sum2 - dice1 - dice2;
+    }
 }
-
+if (sum2 == numbertoplayto) {
+    confirm("Player 2 won!");
+} else if (sum1 == numbertoplayto) {
+    confirm("Player 1 won!");
+}
     if (currentplayer == 1) {
         if (confirm("Player 1 has " + sum1 + "points. Do you want to roll again?")) {
             diceroll(dice1, dice2);
